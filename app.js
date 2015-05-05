99cbf771a8715f5ab212481cb7ff983f8de012ec
Alex Quan <alexquan302@gmail.com>
2015-05-05 18:21:47 -0400

    ClickableMenuFix

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 var main = function() {
   $('.about').click(function() {
-    window.location.replace("http://a4quan.github.io/about/index.html");
+    window.location.replace("http://a4quan.github.io/about");
 
   });
 
   $('.resume').click(function() {
-    window.location.replace("http://a4quan.github.io/about/index.html");
+    window.location.replace("http://a4quan.github.io/resume");
 
   });
 
   $('.contact').click(function() {
-    window.location.replace("http://a4quan.github.io/about/index.html");
+    window.location.replace("http://a4quan.github.io/contact");
 
   });
 
   $('.about').css( 'cursor', 'pointer' );
   $('.resume').css( 'cursor', 'pointer' );
-  $('.control').css( 'cursor', 'pointer' );
+  $('.contact').css( 'cursor', 'pointer' );
 };
 
 $(document).ready(main);
